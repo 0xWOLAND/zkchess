c3661fdf2c11080ccd17b732a64c4f98196f8130
bhargav <bhargav.annem@gmail.com>
2023-09-09 13:08:13 -0500

    remove zketh

--- packages/contracts/scripts/abi.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const { abi } = require('../artifacts/contracts/ZKEth.sol/ZKEth.json')
-
-fs.writeFileSync(path.join(__dirname, '../abi/ZKEth.json'), JSON.stringify(abi))
