43bfe0a99a4a5182c6a71ac2b6375d12bb6f8d43
Chance <jchancehud@gmail.com>
2023-09-09 21:35:43 -0500

    refactor: clean up schema

--- packages/relay/src/schema.mjs
+++ packages/relay/src/schema.mjs
@@ -3,40 +3,6 @@ import { Position } from "kokopu";
 
 export default [
   {
-    name: "Message",
-    primaryKey: "_id",
-    rows: [
-      {
-        name: "_id",
-        type: "String",
-        default: () => nanoid(),
-      },
-      ["FEN", "String"],
-      ["timestamp", "Int", { unique: true }],
-      ["publicSignals", "String"],
-      ["proof", "String"],
-      ["channelName", "String"],
-    ],
-  },
-  {
-    name: "StateTreeRoot",
-    primaryKey: "hash",
-    rows: [["hash", "String"]],
-  },
-  {
-    name: "Match",
-    primaryKey: "matchId",
-    rows: [
-      {
-        name: "matchId",
-        type: "String",
-        default: () => nanoid(),
-      },
-      ["white", "String"],
-      ["black", "String"],
-    ],
-  },
-  {
     name: "Game",
     primaryKey: "_id",
     rows: [
